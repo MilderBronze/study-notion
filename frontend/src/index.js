3a0fff86e3786078600bdb42fc43d06124059b95
milderbronze <sumitinbki@gmail.com>
2024-09-19 10:14:41 +0530

    fixed the import issues in the slice

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-Router";
+import { Provider } from "react-redux";
+import rootReducer from "./reducer";
+import { configureStore } from "@reduxjs/toolkit";
 
 const store = configureStore({
-  reducer: RootReducer, // import this from slice.
+  reducer: rootReducer, // import this from slice.
 });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
